7211fc3f2e59f96fb91794c1e3b62e6cfe25895c
martincooperbiz <141577484+martincooperbiz@users.noreply.github.com>
2023-08-26 01:52:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -83,12 +83,13 @@ def main(start_data, end_data):
 
     ###### SET UP PAGE ######
     icon_path = os.path.join(".", "public", "owl.png")
-    st.set_page_config(page_title="ESG Smart Insight", page_icon=icon_path,
+    st.set_page_config(page_title="OWL AI", page_icon=icon_path,
                        layout='centered', initial_sidebar_state="collapsed")
     _, logo, _ = st.columns(3)
-    logo.image(icon_path, width=600)
-    style = ("text-align:center; padding: -20px; font-family: arial black;, "
-             "font-size: 300%")
+    logo.image(icon_path, width=200)
+    style = ("text-align:center; padding: 0px; font-family: arial black;, "
+             "font-size: 400%")
+    title = f"<h1 style='{style}'>OWL<sup>AI</sup></h1><br><br>"
     st.write(title, unsafe_allow_html=True)
 
 
